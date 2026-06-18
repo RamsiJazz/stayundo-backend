a11dd9a10f84f289a600485a1e8746e1d9425720
Ramsi Jazz <ramsi.jazz@gmail.com>
2026-06-18 08:46:16 +0400

    serializers updated

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "users",
+    "users",'rest_framework','drf_yasg',
 ]
 
 MIDDLEWARE = [
